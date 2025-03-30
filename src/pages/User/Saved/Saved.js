326200b97af37a4f0ba6edf65fc260ede7ea5a3c
Minh-Ha-04 <quangminhh2004@gmail.com>
2025-03-30 11:21:49 +0700

    chỉnh usersaved

--- src/pages/User/Saved/Saved.js
+++ src/pages/User/Saved/Saved.js
@@ -1,11 +1,52 @@
 import styles from './Saved.module.scss';
 import classNames from 'classnames/bind';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookBookmark,faShare } from '@fortawesome/free-solid-svg-icons';
+import Button from '~/components/Button';
+import HotNews from '~/components/HotNews';
 const cx= classNames.bind(styles)
 function Saved() {
     return ( 
-        <div>
-            Save
+        <div className='wrapper'>
+            <div className={cx('title')}>Tin đã lưu</div>
+            <div className={cx('content')}>
+                <div className={cx('item')}><HotNews/>
+                <div className={cx('media')}>
+                        <Button rounded className={cx('media-item')}>
+                            <FontAwesomeIcon icon={faBookBookmark} />
+                            Bỏ lưu
+                        </Button>
+                        <Button rounded className={cx('media-item')}>
+                            <FontAwesomeIcon icon={faShare} />
+                            Chia sẻ
+                        </Button>
+                    </div>
+                </div>
+                <div className={cx('item')}><HotNews/>
+                <div className={cx('media')}>
+                        <Button rounded className={cx('media-item')}>
+                            <FontAwesomeIcon icon={faBookBookmark} />
+                            Lưu bài
+                        </Button>
+                        <Button rounded className={cx('media-item')}>
+                            <FontAwesomeIcon icon={faShare} />
+                            Chia sẻ
+                        </Button>
+                    </div>
+                </div>
+                <div className={cx('item')}><HotNews/>
+                <div className={cx('media')}>
+                        <Button rounded className={cx('media-item')}>
+                            <FontAwesomeIcon icon={faBookBookmark} />
+                            Lưu bài
+                        </Button>
+                        <Button rounded className={cx('media-item')}>
+                            <FontAwesomeIcon icon={faShare} />
+                            Chia sẻ
+                        </Button>
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
